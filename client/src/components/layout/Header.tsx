1061337d2a5f4caa3256e45a40ddaf90bac602c8
Mak Maid Mulabegovic <makmaid.mulabegovic@abc-techgroup.com>
2025-08-08 08:41:52 +0200

    icon search

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -155,7 +155,7 @@ export default function Header() {
               className="p-3 rounded-full text-yellow-500 hover:text-yellow-600 transition-colors duration-300 focus:outline-none"
               aria-label="Search"
             >
-              <Search className="h-6 w-6" />
+              <Search className="h-7 w-7 font-bold" />
             </motion.button>
 
             {/* Language Selector Icon */}
